d1496f184c2082660e636f08bea29d82a1e2a8fc
mdfelchlin <mdfelchlin@gmail.com>
2026-03-09 11:50:52 -0700

    Add event listener for Vite preload error handling
    - Implemented a window event listener to reload the page on 'vite:preloadError' event.
    - This enhancement aims to improve user experience by ensuring the application recovers from preload errors seamlessly.

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './css/global.css'
 
+window.addEventListener('vite:preloadError', () => {
+  window.location.reload()
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
